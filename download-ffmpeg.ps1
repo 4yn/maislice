4139ce51ee56323e1f0b5c668949824d7e1a6d36
4yn <4yn@users.noreply.github.com>
2024-07-26 21:21:44 +0800

    fix missing directory when downloading ffmpeg

--- download-ffmpeg.ps1
+++ download-ffmpeg.ps1
@@ -1,3 +1,4 @@
+mkdir ./public/ffmpeg -Force
 wget https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm/ffmpeg-core.js -OutFile public/ffmpeg/ffmpeg-core.js
 wget https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm/ffmpeg-core.wasm -OutFile public/ffmpeg/ffmpeg-core.wasm
 wget https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm/ffmpeg-core.worker.js -OutFile public/ffmpeg/ffmpeg-core.worker.js
